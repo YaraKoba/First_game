dfc541645eddbfdcc5fa28bc1312dda69bd7e692
YaraKoba <zangetsyyy@mail.ru>
2022-12-02 18:47:55 +0200

    Доработал модельку

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import random
 
 class MoveStars:
     def __init__(self, w, h, sur):
+        self.stars = 50
         self.surface = sur
         self.x_speed = 0
         self.y_speed = 0
@@ -11,12 +12,12 @@ class MoveStars:
         self.max_speed = 40
         self.c_turb = 0.5
         self.WITHE = (255, 255, 255)
-        self.BLUE = (120, 100, 190)
+
         self.speed_x_middle = 0
         self.tmp_mouse_pos = 0
         self.W = w
         self.H = h
-        self.star_pos = {num: [random.randint(1, w), random.randint(1, h)] for num in range(100)}
+        self.star_pos = {num: [random.randint(1, w), random.randint(1, h)] for num in range(self.stars)}
 
     def physics_fly(self, mouse_y):
         # print(self.all_speed, str(self.x_speed)[:-12], str(self.y_speed)[:-12])
@@ -49,8 +50,7 @@ class MoveStars:
                 self.x_speed = (1 - (((self.H / 2) - mouse_y) / (self.H / 2))) * self.all_speed
 
     def draw_stars(self):
-        self.surface.fill(self.BLUE)
-        for star in range(100):
+        for star in range(self.stars):
             x = self.star_pos[star][0]
             y = self.star_pos[star][1]
             if x < 2:
@@ -65,7 +65,7 @@ class MoveStars:
             self.star_pos[star][0] -= self.x_speed
             self.star_pos[star][1] += self.y_speed
             pygame.draw.rect(self.surface, self.WITHE, (x, y, 3, 3))
-        pygame.display.update()
+
 
 
 class Map:
@@ -75,18 +75,18 @@ class Map:
 class Planer:
     def __init__(self, width, height, sc):
         green = (210, 10, 120)
-        self.planer_img = pygame.image.load('plain.png')
+        self.planer_img = pygame.image.load('plain.png').convert_alpha()
+        self.planer_img = pygame.transform.scale(self.planer_img, (200, 100))
+        self.planer_img = pygame.transform.rotate(self.planer_img, -90)
         self.h = height
-        self.planer.fill(green)
         self.sc = sc
         self.rect = self.planer_img.get_rect(center=(width / 2, height / 2))
 
     def show_planer(self, mouse_pos):
-        # corner = self.h / mouse_pos * 180
-        # new_img = pygame.transform.rotate(self.planer, corner)
-        self.planer_img.set_colorkey((255, 255, 255))
-        self.sc.blit(self.planer_img, self.rect)
-        pygame.display.update()
+        corner = mouse_pos / self.h * 180
+        x = pygame.transform.rotate(self.planer_img, corner)
+        self.sc.blit(x, self.rect)
+
 
 
 class GameWindow:
@@ -94,6 +94,7 @@ class GameWindow:
         pygame.init()
         self.W = 1500
         self.H = 600
+        self.color_bg = (120, 100, 190)
         self.mouse_y = self.H / 2
         self.surface = pygame.display.set_mode((self.W, self.H))
         pygame.display.set_caption('Fly GAME')
@@ -109,9 +110,11 @@ class GameWindow:
                 if event.type == pygame.MOUSEMOTION:
                     self.mouse_y = event.pos[1]
 
+            self.surface.fill(self.color_bg)
             self.stars.physics_fly(self.mouse_y)
             self.stars.draw_stars()
             self.planer.show_planer(self.mouse_y)
+            pygame.display.update()
 
             self.cloock.tick(60)
 
